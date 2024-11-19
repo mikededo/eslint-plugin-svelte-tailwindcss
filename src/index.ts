e138bc557e116d23d3f96ac0fb5b85c072605350
Miquel de Domingo <miquelddg@gmail.com>
2024-11-19 12:41:59 +0100

    chore: add ci & other chore things

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import type { ESLint } from "eslint";
+import type { ESLint, Linter } from 'eslint';
 
 import { version } from '../package.json';
+import rules from './rules';
 
 const plugin: ESLint.Plugin = {
   meta: {
-    name: "eslint-plugin-svelte-tailwindcss",
+    name: 'eslint-plugin-svelte-tailwindcss',
     version
-  }
-}
+  },
+  rules
+};
 
 export default plugin;
+
+type RuleDefinitions = typeof rules;
+export type RuleOptions = {
+  [K in keyof RuleDefinitions]: RuleDefinitions[K]['defaultOptions'];
+};
+export type Rules = {
+  [K in keyof RuleDefinitions]: Linter.RuleEntry<RuleOptions[K]>;
+};
 
