ec339f394ea8e64c3969d21772e3975bd92b48f9
Miquel de Domingo <miquelddg@gmail.com>
2024-11-22 11:26:36 +0100

    chore: add coverage utils

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: ['src/**/index.ts'],
+      include: ['src/rules/**/*.ts'],
+      provider: 'v8'
+    },
     globals: true,
     reporters: 'dot'
   }
