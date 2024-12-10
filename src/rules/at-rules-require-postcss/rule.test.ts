6b8d15e77f112ba6cd607fd5805faffdd7f37d6a
Miquel de Domingo <miquelddg@gmail.com>
2024-12-10 13:50:25 +0100

    feat(at-rules-require-postcss): add rule

--- src/rules/at-rules-require-postcss/rule.test.ts
+++ src/rules/at-rules-require-postcss/rule.test.ts
@@ -12,16 +12,133 @@ const tester = new RuleTester({
   }
 });
 
-const ERROR = { message: 'Using \'{{atRule}}\' requires setting style lang to postcss' };
+const ERROR = { message: 'Using \'@apply\' requires setting style lang to postcss' };
 
-tester.run('no-literal-mustache-mix', rule, {
+tester.run('at-apply-require-postcss', rule, {
   invalid: [
     {
-      code: `<style lang="postcss">.foo { .foo { @apply px-8; } }</style>`,
-      errors: [ERROR]
+      code: `
+<style>
+  .foo {
+    @apply flex;
+    .foo {
+      @apply px-8;
+      .foo {
+        @apply px-8;
+      }
+    }
+  }
+</style>
+`,
+      errors: [ERROR],
+      output: `
+<style lang="postcss">
+  .foo {
+    @apply flex;
+    .foo {
+      @apply px-8;
+      .foo {
+        @apply px-8;
+      }
+    }
+  }
+</style>
+`
+    },
+    {
+      code: `
+<div class="foo"></div>
+<style lang="scss">
+  .foo {
+    .foo {
+      .foo {
+        .foo {
+          @apply flex;
+        }
+      }
+    }
+  }
+</style>
+`,
+      errors: [ERROR],
+      output: `
+<div class="foo"></div>
+<style lang="postcss">
+  .foo {
+    .foo {
+      .foo {
+        .foo {
+          @apply flex;
+        }
+      }
+    }
+  }
+</style>
+`
+    },
+    {
+      code: `
+<style>
+  .foo {
+    @ap flex;
+    @app flex;
+    @apply flex;
+  }
+</style>
+`,
+      errors: [ERROR],
+      output: `
+<style lang="postcss">
+  .foo {
+    @ap flex;
+    @app flex;
+    @apply flex;
+  }
+</style>
+`
+    },
+    {
+      code: '<style lang="scss">.foo { @apply flex; }</style>',
+      errors: [ERROR],
+      output: '<style lang="postcss">.foo { @apply flex; }</style>'
+    },
+    {
+      code: '<style lang="css">.foo { @apply flex; }</style>',
+      errors: [ERROR],
+      output: '<style lang="postcss">.foo { @apply flex; }</style>'
     }
   ],
   valid: [
-    { code: '' }
+    { code: '<style lang="postcss"></style>' },
+    { code: '<style lang="postcss">.foo { @apply flex; }</style>' },
+    { code: '<style>.foo { @anything flex; }</style>' },
+    {
+      code: `
+<style lang="postcss">
+  .foo {
+    @apply flex;
+  }
+</style>
+`
+    },
+    {
+      code: `
+<style lang="postcss">
+  .foo {
+    .foo {
+      .foo {
+        .foo {
+          @apply flex;
+        }
+      }
+    }
+  }
+</style>
+`
+    },
+    // StyleContext.status === "no-style-element"
+    { code: '' },
+    // StyleContext.status "unknown-lang"
+    { code: '<style lang="tcss"></style>' }
   ]
 });
