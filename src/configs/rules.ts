651bd3c347f1821283caad6f008d8a590417263c
Miquel de Domingo <miquelddg@gmail.com>
2024-12-10 13:53:47 +0100

    chore: update config & exports

--- src/configs/rules.ts
+++ src/configs/rules.ts
@@ -1,4 +1,5 @@
 export default {
+  'svelte-tailwindcss/at-apply-require-postcss': 'error',
   'svelte-tailwindcss/no-literal-mustache-mix': 'error',
   'svelte-tailwindcss/sort-classes': 'error'
 } as const;
