27640091171c914cb9fb28407b88dc8d833d8b46
Miquel de Domingo <miquelddg@gmail.com>
2024-12-10 13:50:25 +0100

    feat(at-rules-require-postcss): add rule

--- src/rules/at-rules-require-postcss/rule.ts
+++ src/rules/at-rules-require-postcss/rule.ts
@@ -1,3 +1,5 @@
+import type { AST } from 'svelte-eslint-parser';
+
 import { createNamedRule } from '../../utils';
 
 export type OptionList = [];
@@ -5,28 +7,76 @@ export type OptionList = [];
 export type MessageIds = 'require-postcss';
 
 export default createNamedRule<OptionList, MessageIds>({
-  create(_context) {
+  create(rootContext) {
+    const { sourceCode } = rootContext;
+    const context = sourceCode.parserServices.getStyleContext?.();
+    if (!context || context.status !== 'success') {
+      return {};
+    }
+
     return {
-      SvelteStyleElement(node) {
-        console.log(node);
+      SvelteStyleElement(node: AST.SvelteStyleElement) {
+        const startTag = node.startTag;
+        let classAttribute: AST.SvelteAttribute | null = null;
+        const hasPostcssLangAttr = startTag.attributes.some((attr) => {
+          if (attr.type !== 'SvelteAttribute') {
+            return false;
+          }
+
+          classAttribute = attr;
+          const { key, value } = attr;
+          const attrValue = value[0];
+          return key.name === 'lang' && attrValue.type === 'SvelteLiteral' && attrValue.value === 'postcss';
+        });
+
+        if (hasPostcssLangAttr) {
+          return;
+        }
+
+        // Find first at rule, if any
+        // If false, means that @apply has been found
+        const includesApply = context.sourceAst.walk((node) => {
+          if (node.type !== 'atrule') {
+            return undefined;
+          }
+
+          const atName = node.name;
+          if (atName === 'apply') {
+            return false;
+          }
+        });
+        if (includesApply === undefined) {
+          return;
+        }
+
+        rootContext.report({
+          fix: (fixer) => {
+            if (classAttribute) {
+              return fixer.replaceText(classAttribute, 'lang="postcss"');
+            }
+
+            return fixer.insertTextBeforeRange([startTag.range[1] - 1, startTag.range[1]], ' lang="postcss"');
+          },
+          messageId: 'require-postcss',
+          node
+        });
       }
     };
   },
-  defaultOptions: [
-    // Configure here the default options, if any
-  ],
+  defaultOptions: [],
   meta: {
     docs: {
       description: 'undefined'
     },
+    fixable: 'code',
     messages: {
-      'require-postcss': 'Using \'{{atRule}}\' requires setting style lang to postcss'
+      'require-postcss': 'Using \'@apply\' requires setting style lang to postcss'
     },
     schema: [{
       properties: {},
       type: 'object'
     }],
-    type: 'suggestion'
+    type: 'problem'
   },
-  name: 'at-rules-require-postcss'
+  name: 'at-apply-require-postcss'
 });
