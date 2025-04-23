19e122faddfc8c2cb1413e69b4ea68ab2d2896a1
Miquel de Domingo <miquelddg@gmail.com>
2025-04-23 08:36:02 +0200

    test: add e2e for v3 and v4

---
+++ packages/test/v3/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import twMerge from 'tw-merge';
+
+    const merged = twMerge('md:bg-blue-500 w-full h-screen bg-blue-200 bg-blue-200 px-2 py-4');
+    const merged2 = twMerge(
+        'md:bg-blue-500 w-full h-screen bg-blue-200 bg-blue-200 px-2 py-4',
+        'md:bg-blue-500 w-full h-screen bg-blue-200 bg-blue-200 px-2 py-4',
+        'md:bg-blue-500 w-full h-screen bg-blue-200 bg-blue-200 px-2 py-4',
+    );
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="md:bg-blue-500 w-full h-screen bg-blue-200 bg-blue-200 px-2 py-4"></div>
+<div class="md:bg-blue-200 {merged} md:p-8"></div>
+
+<style>
+:root {
+    @apply bg-blue-200;
+}
+</style>
