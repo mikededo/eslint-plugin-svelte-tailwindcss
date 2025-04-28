afe9a3cde03f4f5140bcfb8aa8c1317a47d37c9c
Miquel de Domingo <miquelddg@gmail.com>
2025-04-28 09:57:13 +0200

    test: extend e2e tests

---
+++ packages/test/v3/src/components/button/text-icon-button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { Icon as LucideIcon } from 'lucide-svelte';
+    import type { ComponentType } from 'svelte';
+
+    import type { ButtonProps } from './index.js';
+
+    import { twMerge } from 'tailwind-merge';
+
+    import Button from './button.svelte';
+
+    type Props = {
+        label: string;
+        Icon: ComponentType<LucideIcon>;
+        children?: never;
+    } & ButtonProps;
+    const { Icon, label, ...restProps }: Props = $props();
+
+    const buttonClasses = $derived(
+        twMerge('flex items-center gap-2 h-10 rounded-full text-sm', restProps.class)
+    );
+</script>
+
+<Button {...restProps} class={buttonClasses}>
+    <Icon class="size-4" />
+    <span>{label}</span>
+</Button>
---
+++ packages/test/v4/src/components/container.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    type Props = { children: Snippet; class?: string; }
+    const { children, ...rest }: Props = $props();
+</script>
+
+<div class="container custom-container mx-auto {rest.class} max-w-screen-lg">
+    {@render children()}
+</div>
+
+<style>
+.custom-container {
+    @apply px-4 py-8 md:px-8;
+}
+</style>
